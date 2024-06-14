389c2446d53c6f2527e6067a990f4060b6f8bbfa
ton-toh <mwangiantony068@gmail.com>
2024-06-14 18:46:16 +0300

    updated files

--- plp_ecommerce/views.py
+++ plp_ecommerce/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Product 
+from django.shortcuts import get_object_or_404, render, get_list_or_404
+from .models import Product, Customer 
 
 # Create your views here.
 def product_list(request):
@@ -9,3 +9,23 @@ def product_list(request):
     }
     return render(request, 'plp_ecommerce/product_list.html', context)
     
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        'product': product
+    }
+    return render(request, 'plp_ecommerce/product_detail.html', context)
+
+def customer_list(request):
+    customers = Customer.objects.all()
+    context = {
+        'customers': customers
+    }
+    return render(request, 'plp_ecommerce/customer_list.html', context)
+
+def customer_detail(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    context = {
+        'customer': customer
+    }
+    return render(request, 'plp_ecommerce/customer_detail.html', context)
